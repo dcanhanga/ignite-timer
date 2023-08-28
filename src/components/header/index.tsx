abb8764dbd1d22d5fb84ac47d81fb7b846934537
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-08-28 18:53:15 +0100

    Refactor: fix the import of logo in component Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PiMoonStars, PiScroll, PiSun, PiTimer } from 'react-icons/pi';
 import { NavLink } from 'react-router-dom';
 
+import logoIgnite from '@/assets/ignite.svg';
 import { SwitchThemeContext } from '@/context/switcherTheme';
 
 import * as C from './styles';
@@ -14,7 +15,7 @@ export const Header = () => {
     <C.HeaderContainer>
       <C.SideLeft>
         <span>
-          <img src="/ignite.svg" alt="" />
+          <img src={logoIgnite} alt="" />
         </span>
 
         <C.FlipContainer onClick={switcherTheme}>
