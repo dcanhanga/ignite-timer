fecf80ffb010e25e1994aebe3742e49b2e21dca8
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-08-28 17:01:00 +0100

    Refactor: Adjusting sizes for responsive

--- src/layouts/defaultLayout/styles.ts
+++ src/layouts/defaultLayout/styles.ts
@@ -3,7 +3,7 @@ import { styled } from 'styled-components';
 export const LayoutContainer = styled.div`
   width: min(${props => props.theme.rem(1200)}, 95%);
   border-radius: 8px;
-  height: 100vh;
+  height: calc(100vh + ${props => props.theme.rem(80 * 2)});
   background-color: ${props => props.theme.colors['bg-2']};
   margin: ${props => props.theme.rem(80)} auto;
   padding: ${props => props.theme.rem(40)} ${props => props.theme.rem(16)};
